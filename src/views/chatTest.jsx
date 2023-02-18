56586483dc22f01e0e26a822e6de13d03c66ae0f
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2023-02-18 00:46:26 -0700

    Added more information to message component and created skeleton of chat

--- src/views/chatTest.jsx
+++ src/views/chatTest.jsx
@@ -4,20 +4,27 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import Grid from '@mui/material/Grid';
 import ListItemText from '@mui/material/ListItemText';
 import { ListItem } from '@mui/material';
 
 function Message({props}) {
 
+    const [alignment, setalignment] = useState(props.align == 0 ? "left" : "right");
+    console.log(alignment);
+
     return (
         <div>
-            <Box sx={{ width: '100%', maxWidth: 160, bgcolor: 'background.paper' }}>
+            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                 <List>
-                    <ListItem>
-                        <ListItemButton>
-                            <ListItemText primary={props.message}></ListItemText>
-                        </ListItemButton>
-                    </ListItem>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <ListItemText align={alignment} primary={props.message}></ListItemText>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <ListItemText align={alignment} secondary={props.sender}></ListItemText>
+                        </Grid>
+                    </Grid>
                 </List>
             </Box>
         </div>
@@ -25,8 +32,28 @@ function Message({props}) {
 
 }
 
+
 // After message we create a conversation component that will basically consist of several messages one after another
 // Conversation->message->{sender, data}
+// vetic sector gurgaon 45 
+
+function Chat({props}) {
+
+    return (
+        <div>
+            <List>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Message props={{message: "Hello Human", sender: "robot", align:0}}></Message>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Message props={{message: "Hello Robot", sender: "human", align:1}}></Message>
+                    </Grid>
+                </Grid>
+            </List>
+        </div>
+    )
+}
 
 
 export const ChatTest = () => {
@@ -35,7 +62,8 @@ export const ChatTest = () => {
           {/* <Title />
           <MessageList />
           <SendMessageForm /> */}
-          <Message props={{message: "Hello World"}}></Message>
+          {/* <Message props={{message: "Hello World"}}></Message> */}
+          <Chat></Chat>
        </div>
     )
 }
